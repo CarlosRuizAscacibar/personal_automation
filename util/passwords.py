476fb0d0a844198aca50cd86e63eb1adbaf1bd7c
Carlos Ruiz <ruizcarlos93@yahoo.es>
2019-09-14 21:09:24 +0200

    password was dirty from reading

--- util/passwords.py
+++ util/passwords.py
@@ -4,7 +4,7 @@ import os
 def open_vault():
     p = os.path.dirname(os.path.realpath(__file__))
     # password is in a file inside computer and service
-    vault_pass = open(p + '/../pass.txt', 'r', encoding='utf-8').readline()
+    vault_pass = open(p + '/../pass.txt', 'r', encoding='utf-8').readline().strip()
     vault = pykeepass.PyKeePass(p + '/../vault.kdbx', password=vault_pass)
     return vault
 
